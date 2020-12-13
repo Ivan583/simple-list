ea7002e3c65c738f423221df6c460393039ce4b5
Ivan583 <ivanych2293@gmail.com>
2020-12-13 17:02:13 +0300

    Оформление элемента списка

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,5 +1,29 @@
 <template>
-  <li>{{ elem }}</li>
+  <li>
+    <div class="button-left">
+      <button class="button start">Start</button>
+      <button class="button finish">Finish</button>
+    </div>
+
+    <div class="button-right">
+      <button class="button edit">Edit</button>
+      <button class="button delete">Delete</button>
+    </div>
+
+    <div class="row">
+      <div class="index">
+        <span>{{ elem.id }}</span>
+      </div>
+
+      <div class="title">
+        <span>{{ elem.title }}</span>
+      </div>
+
+      <div class="description">
+        <span>{{ elem.description }}</span>
+      </div>
+    </div>
+  </li>
 </template>
 
 <script>
